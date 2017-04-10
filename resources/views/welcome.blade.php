0f59d81045cb6069f172e6afdb88d3190dbfe27d
josephhwee <josephwee@outlook.com>
2017-04-11 08:00:21 +1000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">The beautiful Laravel</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">Learning laravel</h1>
+            <p>This blog post will get you on the right track with Laravel!</p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">The next steps</h1>
+            <p>Understanding the Basics is great, but you need to be able to make the next steps.</p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">Laravel 5.3</h1>
+            <p>Though announced as a "minor release", Laravel 5.3 ships with some very interesting additions and
+                features</p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
+@endsection
