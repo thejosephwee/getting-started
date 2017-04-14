b32c335dd2b78be4767a3da6a853f000edb98596
josephhwee <josephwee@outlook.com>
2017-04-14 19:36:09 +1000

    Playing with routes

---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master)
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">About</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <p>Blah Blah Blah
+            </p>
+        </div>
+    </div>
+@endsection
