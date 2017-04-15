4bbb61490e02b22f1ff68a7475263f3085db14bc
josephhwee <josephwee@outlook.com>
2017-04-15 14:03:56 +1000

    Added in better routes

--- resources/views/other/about.blade.php
+++ resources/views/other/about.blade.php
@@ -3,12 +3,17 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <p class="quote">About</p>
+            <p class="quote">About Me</p>
         </div>
     </div>
     <div class="row">
         <div class="col-md-12">
-            <p>Blah Blah Blah
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda commodi illum nobis nostrum numquam
+                officiis possimus provident rem repellat sint? Dicta eligendi eum hic, labore nisi non quidem quos
+                voluptates.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa debitis magni modi nihil numquam odit
+                pariatur recusandae repellendus sint voluptates. Blanditiis expedita minus nostrum numquam placeat porro
+                praesentium quae soluta!
             </p>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,37 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/',function(){
     return view('blog.index');
-});
+})->name('blog.index');
 
-Route::get('post/{id}', function(){
-    return view('blog.post');
-});
+Route:get('post/{id}',function(){
+  return view('blog.post');
+})->name('blog.post');
 
 Route::get('about',function(){
-   return view('other.about');
-});
+    return view('other.about');
+})->name('other.about)');
 
-Route::get('admin',function(){
-   return view('admin.index');
-});
+Route::group(['prefix' => 'admin'],function() {
 
-Route::get('admin/create', function(){
-   return view('admin.create');
-});
+    Route::get('', function () {
+        return view('admin.index');
+    })->name('admin.index');
 
-Route:;get('admin/edit/{id}',function(){
-   return view('admin.edit');
+    Route::get('create',function(){
+       return view('admin.create');
+    })->name('admin.create');
+
+    Route::post('create',function(){
+       return "It works!";
+    })->name('admin.create');
+
+    Route::get('edit/{id}', function () {
+        return view('admin.edit');
+    })->name('admin.edit');
+
+    Route::post('edit', function(){
+        return "It works!";
+    })->name('admin.update');
 });
